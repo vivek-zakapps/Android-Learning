fb37302e61f32ac17e702d81b49dac86afe03867
vivek-zakapps <vivek_k@zakapps.com>
2024-04-01 23:24:49 +0530

    Passing data between fragments-vivek ⚔️

--- app/src/main/java/com/example/androidtutoriallearning/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/androidtutoriallearning/fragments/FirstFragment.kt
@@ -1,67 +1,105 @@
 package com.example.androidtutoriallearning.fragments
 
+import SecondFragment
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.androidtutoriallearning.R
 import com.example.androidtutoriallearning.interfaces.DataPasserInterface
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FirstFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 open class FirstFragment : Fragment(R.layout.fragment_first) {
-
 
     private var lvInterface: DataPasserInterface? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("FirstFragment", "onCreate")
+
+
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val first: TextView = view.findViewById(R.id.fragment_1_text)
-        val data = arguments?.getInt("data")
-        first.text = data.toString()
+        Log.d("FirstFragment", "onViewCreated")
 
-        // Notify data has passed
-        lvInterface?.onDataPassed(data, "FirstFragment")
+        val button = view.findViewById<Button>(R.id.fr_btn_1)
+        button.setOnClickListener {
+            println("hhhhh")
+            lvInterface?.onDataPassed( "FirstFragmentButtonPressed")
+
+        }
+
+        try {
+           val data= arguments?.getString("data")
+            view.findViewById<TextView>(R.id.fragment_1_text)?.text = data
+
+
+
+        } catch (e:Exception){
+            println(e.toString())
+        }
+
+
+//        val view = view.findViewById<TextView>(R.id.fragment_1_text);
+//        view.text = "Navigated back from fragment 2! Supper"
 
     }
 
 
     override fun onStart() {
+        println("sttattatatrt")
         super.onStart()
+        Log.d("FirstFragment", "onStart")
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("FirstFragment", "onResume")
+
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("FirstFragment", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("FirstFragment", "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d("FirstFragment", "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("FirstFragment", "onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("FirstFragment", "onDetach")
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         try {
             lvInterface = context as DataPasserInterface
-
         } catch (c: ClassCastException) {
             throw ClassCastException("$context must implement DataPassListener")
         } catch (e: Exception) {
-            println(e.toString())
+            Log.e("FirstFragment", "Error: ${e.toString()}")
         }
+        Log.d("FirstFragment", "onAttach")
     }
-
-
-
-    private fun onclickListener() {
-        button?.setOnClickListener {
-            lvInterface?.onDataPassed("Hello Fragment 1")
-        }
-    }
-
-
-    fun getDataFromActivity(msg: String) {
-        first?.text = msg
-    }
-
 }
--- app/src/main/java/com/example/androidtutoriallearning/fragments/SecondFragment.kt
+++ app/src/main/java/com/example/androidtutoriallearning/fragments/SecondFragment.kt
@@ -1,6 +1,8 @@
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.androidtutoriallearning.R
@@ -17,19 +19,54 @@ open class SecondFragment : Fragment(R.layout.fragment_second) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val textView: TextView = view.findViewById(R.id.fragment_2_text)
-        val arguments = requireArguments()
-        val data = arguments.getInt("data")
-        textView.text = data.toString()
+        val button = view.findViewById<Button>(R.id.fr_btn_2)
+        button.setOnClickListener {
+            println("hhhhh")
+            lvInterface?.onDataPassed("SecondFragmentButtonPressed")
 
-        // Notify data has passed
-        lvInterface?.onDataPassed(data, "SecondFragment")
+        }
+
+        val data= arguments?.getString("data")
+        view.findViewById<TextView>(R.id.fragment_2_text)?.text = data
+
 
     }
 
 
     override fun onStart() {
+        println("sttattatatrt")
         super.onStart()
+        Log.d("FirstFragment", "onStart - 2")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("FirstFragment", "onResume - 2")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("FirstFragment", "onPause - 2")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("FirstFragment", "onStop - 2")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d("FirstFragment", "onDestroyView - 2")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("FirstFragment", "onDestroy - 2")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("FirstFragment", "onDetach - 2")
     }
 
     override fun onAttach(context: Context) {
